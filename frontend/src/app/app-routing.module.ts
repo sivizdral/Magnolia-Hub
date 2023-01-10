12ccac813cd7bdb8fa1b3dfc664f87f4db4001eb
sivizdral <ivancvetic@outlook.com>
2023-01-10 11:48:33 +0100

    Added front page sketch

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PasswordChangeComponent } from './password-change/password-change.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
   {path: "signup", component: RegisterComponent},
   {path: "password-reset/:userId/:token", component:PasswordResetComponent},
